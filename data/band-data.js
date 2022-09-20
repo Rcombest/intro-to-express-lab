75f60b850aafd029e679ad44daaaba51b340d851
Ryan Combest <crcombest@gmail.com>
2022-09-19 19:13:59 -0500

    adds bands array and exports bands

---
+++ data/band-data.js
@@ -0,0 +1,11 @@
+const bands = [
+  {text: 'Paramore', seen: true},
+  {text: 'mewithoutYou', seen: true},
+  {text: 'Smash Mouth', seen: false},
+  {text: 'Huey Lewis and The News', seen: false}
+
+]
+
+export {
+  bands
+}
